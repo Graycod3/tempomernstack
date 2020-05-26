1d1cebf3de73922afd8a6f521d26149c67b2a17c
Graycod3 <bngorima2@gmail.com>
2020-05-26 23:56:59 +0200

    save

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const routes = require('./routes/api')
 //process.env.MONGODB_URI ||
 // ||"mongodb://localhost/mern_tempo"
 
-mongoose.connect(process.env.MONGODB_URI,{
+mongoose.connect("mongodb://heroku_hgwl82kv:df3SbjMq5uEt%40Y@ds129315.mlab.com:29315/heroku_hgwl82kv",{uri_decode_auth:true},{
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).catch(err => console.log(err));
